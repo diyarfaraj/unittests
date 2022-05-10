39e4106a277940871ba145096727d6b585a0d1d7
Diyar Faraj <diyar.faraj@gmail.com>
2022-05-10 23:04:47 +0200

    init

---
+++ Controllers/BuggyControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ecommerceApi.Controllers;
+using Xunit;
+
+namespace ecommerceApiTest.Controllers
+{
+    public class BuggyControllerTests
+    {
+
+        [Fact]
+        public void GetUnAuthorised_returnUnauthrizedError()
+        {
+            var controller = new BuggyController();
+            var result = controller.GetUnAuthorised();
+
+            Assert.NotNull( result);
+        }
+
+        [Fact]
+        public void GetNotFound_returnNotFoundResult()
+        {
+            var controller = new BuggyController();
+            var result = controller.GetNotFound();
+
+            Assert.NotNull(result);
+        }
+    }
+}
